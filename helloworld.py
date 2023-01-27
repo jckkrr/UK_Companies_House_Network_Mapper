16830a04b51992dba149f346f6d1d58da9a014c6
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-01-27 14:58:17 +1100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+### streamlit run "C:\Users\Jack\Documents\Python_projects\uk_companies_house_byAPI\streamlit_app.py"
+
+#from bs4 import BeautifulSoup
+#import datetime
+#import json
+#import pandas as pd
+#import os
+#from requests import get
+#import re
+#import requests
+import streamlit as st
+#import pytz
+
+
+st.write('hello world')
