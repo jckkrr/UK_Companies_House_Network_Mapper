b34bfe963a12a8f9b10c31a7123a2679c25205fb
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-01-27 15:24:07 +1100

    Add files via upload

---
+++ officer_connections.py
@@ -0,0 +1,209 @@
+## streamlit run "C:\Users\Jack\Documents\Python_projects\uk_companies_house_byAPI\streamlit_app.py"
+
+import pandas as pd
+from requests import get
+import re
+import requests
+import streamlit as st
+
+### functions
+
+def unpack_json_into_dataframe(v):
+    
+    df = pd.DataFrame()
+
+    def unpack_json(v, prefix=''):
+
+        if isinstance(v, dict):
+            for k, v2 in v.items():
+                p2 = "{}['{}']".format(prefix, k)
+
+                px = re.findall("(\w+)",p2)
+                px = ':'.join(px).strip()
+
+                unpack_json(v2, px)
+
+        elif isinstance(v, list):
+            for i, v2 in enumerate(v):
+                p2 = "{}[{}]".format(prefix, i)
+
+                pn = re.findall("\[(.*)\]",p2)[0]
+                px = re.sub("\[(.*)\]", f':{pn}', p2)
+
+                unpack_json(v2, px)
+
+        else:
+        
+            val = repr(v)[1:-1] if repr(v)[0] == "'" and repr(v)[-1] == "'" else repr(v)
+            df.loc[0, prefix] = val
+                    
+    unpack_json(v)
+    
+    return df
+
+###
+
+def findPersonListings(search_term):
+    
+    search_url = f"https://api.company-information.service.gov.uk/search/officers"
+    params={'q':search_term}
+    company_response = requests.get(search_url, auth=(api_key, ''), params=params)
+    js = company_response.json() 
+            
+    df = pd.DataFrame()
+    for item in js['items']:
+        dfx = unpack_json_into_dataframe(item)
+        df = pd.concat([df,dfx])
+    df = df.reset_index(drop=True)
+    
+    try:
+        df['officer_id'] = df['links:self'].apply(lambda x: re.findall('/officers/(.*)/appoint',x)[0]) #.group(1)
+    except:
+        df['officer_id'] = 'X'
+    
+    first_cols = ['title', 'kind', 'date_of_birth:year', 'date_of_birth:month', 'appointment_count', 'officer_id'] + [x for x in df.columns if 'address' in x]
+    ordered_cols = [x for x in first_cols if x in df.columns] + [x for x in df.columns if x not in first_cols]
+    df = df[ordered_cols]
+    
+    return df
+
+
+#####
+
+def getOfficerAppointments(officer_ids):
+    
+    df = pd.DataFrame()
+    
+    for officer_id in officer_ids:
+
+        url = f'https://api.company-information.service.gov.uk/officers/{officer_id}/appointments'
+        response = requests.get(url, auth=(api_key, ''))
+        js = response.json()
+
+
+        for item in js['items']:
+            dfx = unpack_json_into_dataframe(item)
+            df = pd.concat([df,dfx])
+
+        df = df.reset_index(drop=True)
+
+        for key in ['date_of_birth', 'kind', 'is_corporate_officer', 'links']:        
+            if key in js.keys():            
+                if type(js[key]) == dict:
+                    for subkey in js[key].keys():
+                        df[f'{key.strip()}:{subkey.strip()}'] = js[key][subkey]
+                else:
+                    df[key.strip()] = js[key]
+
+        try:
+            df['officer_id'] = df['links:self'].apply(lambda x: re.findall('/officers/(.*)/appoint',x)[0]) #.group(1)
+        except:
+            df['officer_id'] = 'X'
+            
+            
+        for col in ['date_of_birth:year', 'date_of_birth:month']:
+            if col in df.columns:
+                df[col] = df[col].fillna(0)
+                df[col] = df[col].astype(int)
+
+        first_cols = ['name', 'nationality', 'date_of_birth:year', 'date_of_birth:month', 'officer_id', 'officer_role', 'occupation', 'appointed_to:company_name', 'appointed_to:company_number', 'appointed_on',] + [x for x in df.columns if 'address' in x]
+        ordered_cols = [x for x in first_cols if x in df.columns] + [x for x in df.columns if x not in first_cols]
+        df = df[ordered_cols]
+    
+    return df
+
+
+####
+
+def getCompanyPeople(company_numbers):
+    
+    first_columns = ['name', 'person_type', 'officer_role', 'date_of_birth:year', 'date_of_birth:month', 'nationality', 'country_of_residence', 'occupation']
+    df = pd.DataFrame(columns=first_columns)
+    
+    for company_number in company_numbers:
+        
+        st.write(company_number)
+    
+        ### Get Officers ###
+
+        company_url = f"https://api.companieshouse.gov.uk/company/{company_number}/officers"
+        company_response = requests.get(company_url, auth=(api_key, '')).json()
+
+        for item in company_response['items']:
+            dfx = unpack_json_into_dataframe(item)
+            dfx['person_type'] = 'Officer'
+            df = pd.concat([df, dfx])
+
+
+        ### Get PSCS ###
+
+        company_url = f"https://api.companieshouse.gov.uk/company/{company_number}/persons-with-significant-control"
+        company_response = requests.get(company_url, auth=(api_key, '')).json()
+
+        for item in company_response['items']:
+            dfx = unpack_json_into_dataframe(item)
+            dfx['person_type'] = 'PSC'
+            df = pd.concat([df, dfx])
+    
+    return df
+
+
+##################################################
+
+
+st.write('Constituent Investigative Analytics Studio')
+
+st.write('# UK COMPANIES HOUSE OFFICER CONNECTIONS CHECKER #####')
+
+api_key = st.text_input('Please enter API key')
+
+if len(api_key) > 10:
+
+    search_term = st.text_input('Who would like to search for?', 'lydia alexandra gordon')
+
+    if len(search_term) > 0:
+
+        dfSEARCH = findPersonListings(search_term)
+        st.dataframe(dfSEARCH)
+
+        selected_options = st.multiselect(
+            'By row number, select which records that you would like to include in search:',
+            [x for x in range(0, dfSEARCH.shape[0])], [0])
+
+        selected_options_byname = dfSEARCH.loc[selected_options, 'title'].to_list()
+
+        st.write('Selected:', ', '.join(selected_options_byname))
+
+        proceed = st.radio('Ready to go?', ['no','yes'], horizontal=True)
+
+        if proceed == 'yes':
+
+            st.write('Getting data ...')
+
+            officer_ids = dfSEARCH.loc[selected_options, 'officer_id'].to_list()
+
+            st.write(officer_ids)
+
+            dfAPPOINTMENTS = getOfficerAppointments(officer_ids)
+            st.dataframe(dfAPPOINTMENTS)
+
+            company_numbers = dfAPPOINTMENTS['appointed_to:company_number']
+
+            if len(company_numbers) > 0:
+
+                st.write('Searching for who is involved with these companies:')
+
+                dfCOMPANYPEOPLE = getCompanyPeople(company_numbers)
+
+                all_individuals = list(dfCOMPANYPEOPLE['name'].unique())
+
+                st.write('These are the people connected to the person(s) who searched for:')
+                st.write(all_individuals)
+
+                st.write('More details:')
+                st.dataframe(dfCOMPANYPEOPLE)
+                
+                
+st.write('')
+st.write('')
+st.write('&#11041; More tools at www.constituent.au')
