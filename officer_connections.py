dd0d659784cdf9321a1f6f311bf2a75b0331fc36
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-02-02 17:33:48 +1100

    Add files via upload

--- officer_connections.py
+++ officer_connections.py
@@ -334,7 +334,7 @@ if len(api_key) > 10:
                         target_node_color = 'rgba(0, 150, 100, 0.5)'
                         g.add_node(target, color=target_node_color, size=target_node_size, shape='dot', title=target, font=(f'12 Manrope rgba(22,22,22,1)'),)
                         
-                        g.add_edge(source, target, weight=5, title='x', color='rgba(122,122,122,0.6)')
+                        g.add_edge(source, target, weight=5, title='x', color='rgba(122,122,122,0.6)', weight=1)
                     
                     
                     ### display
@@ -353,7 +353,7 @@ if len(api_key) > 10:
                     
                     #components.iframe(f'temp.html')
                     
-                #makePlotPYVIS(dfCOMPANYPEOPLE, 'company_name')
+                makePlotPYVIS(dfCOMPANYPEOPLE, 'company_name')
                 
                 
                 
